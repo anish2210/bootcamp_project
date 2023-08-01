7faeebbc2b55c5a23a44ae6e198e213787e16d51
anish2210 <anishjaiswal1220@gmail.com>
2023-08-01 21:27:04 +0530

    Bootcamp project

---
+++ script.js
@@ -0,0 +1,7 @@
+const forms = document.querySelectorAll('form');
+forms.forEach(form => {
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        alert('Form submitted!');
+    });
+});
